a96618d8b843694c26dedf82ff904f83ff2ea283
Chippa Saiteja <csaiteja64@gmail.com>
2024-12-11 01:16:49 +0530

    committing color kit folder

---
+++ color kit/script.js
@@ -0,0 +1,316 @@
+document.addEventListener('keydown', function (e) {
+    console.log(`Entered character : ${e.key}`);
+    var block = document.getElementById("display");
+    if (e.code === "KeyQ") {
+        const colorName = document.getElementById("colorName");
+        colorName.innerText = "QuartzPink, currently not displaying";
+        const key = document.getElementById("q");
+        block.style.backgroundColor = "QuartzPink ";
+        key.style.backgroundColor = "white";
+        setTimeout(() => {
+            block.style.backgroundColor = "white"
+            key.style.backgroundColor = "#cfaaaa";
+            colorName.innerText = "Enter alphabets on keyboards";
+        }, 4000);
+    }
+    else if (e.code === "KeyW") {
+        const colorName = document.getElementById("colorName");
+        colorName.innerText = "Wheat";
+        const key = document.getElementById("w");
+        block.style.backgroundColor = "Wheat";
+        key.style.backgroundColor = "white";
+        setTimeout(() => {
+            block.style.backgroundColor = "white"
+            key.style.backgroundColor = "#cfaaaa";
+            colorName.innerText = "Enter alphabets on keyboards";
+        }, 4000);
+    }
+    else if (e.code === "KeyE") {
+        const colorName = document.getElementById("colorName");
+        colorName.innerText = "Eggshell, currently not displaying";
+        const key = document.getElementById("e");
+        block.style.backgroundColor = "Eggshell";
+        key.style.backgroundColor = "white";
+        setTimeout(() => {
+            block.style.backgroundColor = "white"
+            key.style.backgroundColor = "#cfaaaa";
+            colorName.innerText = "Enter alphabets on keyboards";
+        }, 4000);
+    }
+    else if (e.code === "KeyR") {
+        const colorName = document.getElementById("colorName");
+        colorName.innerText = "Red";
+        const key = document.getElementById("r");
+        block.style.backgroundColor = "Red";
+        key.style.backgroundColor = "white";
+        setTimeout(() => {
+            block.style.backgroundColor = "white"
+            key.style.backgroundColor = "#cfaaaa";
+            colorName.innerText = "Enter alphabets on keyboards";
+        }, 4000);
+    }
+    else if (e.code === "KeyT") {
+        const colorName = document.getElementById("colorName");
+        colorName.innerText = "Teal";
+        const key = document.getElementById("t");
+        block.style.backgroundColor = "Teal";
+        key.style.backgroundColor = "white";
+        setTimeout(() => {
+            block.style.backgroundColor = "white"
+            key.style.backgroundColor = "#cfaaaa";
+            colorName.innerText = "Enter alphabets on keyboards";
+        }, 4000);
+    }
+    else if (e.code === "KeyY") {
+        const colorName = document.getElementById("colorName");
+        colorName.innerText = "Yellow";
+        const key = document.getElementById("y");
+        block.style.backgroundColor = "Yellow";
+        key.style.backgroundColor = "white";
+        setTimeout(() => {
+            block.style.backgroundColor = "white"
+            key.style.backgroundColor = "#cfaaaa";
+            colorName.innerText = "Enter alphabets on keyboards";
+        }, 4000);
+    }
+    else if (e.code === "KeyU") {
+        const colorName = document.getElementById("colorName");
+        colorName.innerText = "Ultramarine, currently not displaying";
+        const key = document.getElementById("u");
+        block.style.backgroundColor = "Ultramarine";
+        key.style.backgroundColor = "white";
+        setTimeout(() => {
+            block.style.backgroundColor = "white"
+            key.style.backgroundColor = "#cfaaaa";
+            colorName.innerText = "Enter alphabets on keyboards";
+        }, 4000);
+    }
+    else if (e.code === "KeyI") {
+        const colorName = document.getElementById("colorName");
+        colorName.innerText = "Indigo";
+        const key = document.getElementById("i");
+        block.style.backgroundColor = "Indigo";
+        key.style.backgroundColor = "white";
+        setTimeout(() => {
+            block.style.backgroundColor = "white"
+            key.style.backgroundColor = "#cfaaaa";
+            colorName.innerText = "Enter alphabets on keyboards";
+        }, 4000);
+    }
+    else if (e.code === "KeyO") {
+        const colorName = document.getElementById("colorName");
+        colorName.innerText = "Orange";
+        const key = document.getElementById("o");
+        block.style.backgroundColor = "Orange";
+        key.style.backgroundColor = "white";
+        setTimeout(() => {
+            block.style.backgroundColor = "white"
+            key.style.backgroundColor = "#cfaaaa";
+            colorName.innerText = "Enter alphabets on keyboards";
+        }, 4000);
+    }
+    else if (e.code === "KeyP") {
+        const colorName = document.getElementById("colorName");
+        colorName.innerText = "Purple";
+        const key = document.getElementById("p");
+        block.style.backgroundColor = "Purple";
+        key.style.backgroundColor = "white";
+        setTimeout(() => {
+            block.style.backgroundColor = "white"
+            key.style.backgroundColor = "#cfaaaa";
+            colorName.innerText = "Enter alphabets on keyboards";
+        }, 4000);
+    }
+    else if (e.code === "KeyA") {
+        const colorName = document.getElementById("colorName");
+        colorName.innerText = "AntiqueWhite";
+        const key = document.getElementById("a");
+        block.style.backgroundColor = "AntiqueWhite";
+        key.style.backgroundColor = "white";
+        setTimeout(() => {
+            block.style.backgroundColor = "white"
+            key.style.backgroundColor = "#cfaaaa";
+            colorName.innerText = "Enter alphabets on keyboards";
+        }, 4000);
+    }
+    else if (e.code === "KeyS") {
+        const colorName = document.getElementById("colorName");
+        colorName.innerText = "SkyBlue";
+        const key = document.getElementById("s");
+        block.style.backgroundColor = "SkyBlue";
+        key.style.backgroundColor = "white";
+        setTimeout(() => {
+            block.style.backgroundColor = "white"
+            key.style.backgroundColor = "#cfaaaa";
+            colorName.innerText = "Enter alphabets on keyboards";
+        }, 4000);
+    }
+    else if (e.code === "KeyD") {
+        const colorName = document.getElementById("colorName");
+        colorName.innerText = "DarkGreen";
+        const key = document.getElementById("d");
+        block.style.backgroundColor = "DarkGreen";
+        key.style.backgroundColor = "white";
+        setTimeout(() => {
+            block.style.backgroundColor = "white"
+            key.style.backgroundColor = "#cfaaaa";
+            colorName.innerText = "Enter alphabets on keyboards";
+        }, 4000);
+    }
+    else if (e.code === "KeyF") {
+        const colorName = document.getElementById("colorName");
+        colorName.innerText = "FireBrick";
+        const key = document.getElementById("f");
+        block.style.backgroundColor = "FireBrick";
+        key.style.backgroundColor = "white";
+        setTimeout(() => {
+            block.style.backgroundColor = "white"
+            key.style.backgroundColor = "#cfaaaa";
+            colorName.innerText = "Enter alphabets on keyboards";
+        }, 4000);
+    }
+    else if (e.code === "KeyG") {
+        const colorName = document.getElementById("colorName");
+        colorName.innerText = "Gold";
+        const key = document.getElementById("g");
+        block.style.backgroundColor = "Gold";
+        key.style.backgroundColor = "white";
+        setTimeout(() => {
+            block.style.backgroundColor = "white"
+            key.style.backgroundColor = "#cfaaaa";
+            colorName.innerText = "Enter alphabets on keyboards";
+        }, 4000);
+    }
+    else if (e.code === "KeyH") {
+        const colorName = document.getElementById("colorName");
+        colorName.innerText = "HotPink";
+        const key = document.getElementById("h");
+        block.style.backgroundColor = "HotPink";
+        key.style.backgroundColor = "white";
+        setTimeout(() => {
+            block.style.backgroundColor = "white"
+            key.style.backgroundColor = "#cfaaaa";
+            colorName.innerText = "Enter alphabets on keyboards";
+        }, 4000);
+    }
+    else if (e.code === "KeyJ") {
+        const colorName = document.getElementById("colorName");
+        colorName.innerText = "Jet, currently not displaying";
+        const key = document.getElementById("j");
+        block.style.backgroundColor = "Jet";
+        key.style.backgroundColor = "white";
+        setTimeout(() => {
+            block.style.backgroundColor = "white"
+            key.style.backgroundColor = "#cfaaaa";
+            colorName.innerText = "Enter alphabets on keyboards";
+        }, 4000);
+    }
+    else if (e.code === "KeyK") {
+        const colorName = document.getElementById("colorName");
+        colorName.innerText = "Khaki";
+        const key = document.getElementById("k");
+        block.style.backgroundColor = "Khaki";
+        key.style.backgroundColor = "white";
+        setTimeout(() => {
+            block.style.backgroundColor = "white"
+            key.style.backgroundColor = "#cfaaaa";
+            colorName.innerText = "Enter alphabets on keyboards";
+        }, 4000);
+    }
+    else if (e.code === "KeyL") {
+        const colorName = document.getElementById("colorName");
+        colorName.innerText = "Lime";
+        const key = document.getElementById("l");
+        block.style.backgroundColor = "Lime";
+        key.style.backgroundColor = "white";
+        setTimeout(() => {
+            block.style.backgroundColor = "white"
+            key.style.backgroundColor = "#cfaaaa";
+            colorName.innerText = "Enter alphabets on keyboards";
+        }, 4000);
+    }
+    else if (e.code === "KeyZ") {
+        const colorName = document.getElementById("colorName");
+        colorName.innerText = "Zaffre, currently not displaying";
+        const key = document.getElementById("z");
+        block.style.backgroundColor = "Zaffre";
+        key.style.backgroundColor = "white";
+        setTimeout(() => {
+            block.style.backgroundColor = "white"
+            key.style.backgroundColor = "#cfaaaa";
+            colorName.innerText = "Enter alphabets on keyboards";
+        }, 4000);
+    }
+    else if (e.code === "KeyX") {
+        const colorName = document.getElementById("colorName");
+        colorName.innerText = "Xanthic, currently not displaying";
+        const key = document.getElementById("x");
+        block.style.backgroundColor = "Xanthic";
+        key.style.backgroundColor = "white";
+        setTimeout(() => {
+            block.style.backgroundColor = "white"
+            key.style.backgroundColor = "#cfaaaa";
+            colorName.innerText = "Enter alphabets on keyboards";
+        }, 4000);
+    }
+    else if (e.code === "KeyC") {
+        const colorName = document.getElementById("colorName");
+        colorName.innerText = "Chocolate";
+        const key = document.getElementById("c");
+        block.style.backgroundColor = "Chocolate";
+        key.style.backgroundColor = "white";
+        setTimeout(() => {
+            block.style.backgroundColor = "white"
+            key.style.backgroundColor = "#cfaaaa";
+            colorName.innerText = "Enter alphabets on keyboards";
+        }, 4000);
+    }
+    else if (e.code === "KeyV") {
+        const colorName = document.getElementById("colorName");
+        colorName.innerText = "Violet";
+        const key = document.getElementById("v");
+        block.style.backgroundColor = "Violet";
+        key.style.backgroundColor = "white";
+        setTimeout(() => {
+            block.style.backgroundColor = "white"
+            key.style.backgroundColor = "#cfaaaa";
+            colorName.innerText = "Enter alphabets on keyboards";
+        }, 4000);
+    }
+    else if (e.code === "KeyB") {
+        const colorName = document.getElementById("colorName");
+        colorName.innerText = "Blue";
+        const key = document.getElementById("b");
+        block.style.backgroundColor = "Blue";
+        key.style.backgroundColor = "white";
+        setTimeout(() => {
+            block.style.backgroundColor = "white"
+            key.style.backgroundColor = "#cfaaaa";
+            colorName.innerText = "Enter alphabets on keyboards";
+        }, 4000);
+    }
+    else if (e.code === "KeyN") {
+        const colorName = document.getElementById("colorName");
+        colorName.innerText = "Navy";
+        const key = document.getElementById("n");
+        block.style.backgroundColor = "Navy";
+        key.style.backgroundColor = "white";
+        setTimeout(() => {
+            block.style.backgroundColor = "white"
+            key.style.backgroundColor = "#cfaaaa";
+            colorName.innerText = "Enter alphabets on keyboards";
+        }, 4000);
+    }
+    else if (e.code === "KeyM") {
+        const colorName = document.getElementById("colorName");
+        colorName.innerText = "Maroon";
+        const key = document.getElementById("m");
+        block.style.backgroundColor = "Maroon";
+        key.style.backgroundColor = "white";
+        setTimeout(() => {
+            block.style.backgroundColor = "white"
+            key.style.backgroundColor = "#cfaaaa";
+            colorName.innerText = "Enter alphabets on keyboards";
+        }, 4000);
+    }
+});
